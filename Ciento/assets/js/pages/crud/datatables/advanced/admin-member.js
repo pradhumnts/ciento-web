1ecf5df95fbaf3335ba658990abae0884fcbc9be
pradhumnts <pradhumnts@gmail.com>
2021-10-10 09:40:41 +0530

    Frontend Ciento

---
+++ Ciento/assets/js/pages/crud/datatables/advanced/admin-member.js
@@ -0,0 +1,89 @@
+"use strict";
+var KTDatatablesAdvancedColumnRenderingAdminMember = function() {
+
+	var init = function() {
+		var table = $('#admin-member');
+
+		// begin first table
+		table.DataTable({
+			responsive: true,
+			paging: true,
+			columnDefs: [
+				
+				{
+					targets: 1,
+					render: function(data, type, full, meta) {
+						return '<a class="text-dark-50 text-hover-primary" href="mailto:' + data + '">' + data + '</a>';
+					},
+				},
+				{
+					targets: -1,
+					title: 'Actions',
+					orderable: false,
+					render: function(data, type, full, meta) {
+						return '\
+							<div class="dropdown dropdown-inline">\
+								<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" data-toggle="dropdown">\
+	                                <i class="la la-cog"></i>\
+	                            </a>\
+							  	<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
+									<ul class="nav nav-hoverable flex-column">\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-edit"></i><span class="nav-text">Edit Details</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-leaf"></i><span class="nav-text">Update Status</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-print"></i><span class="nav-text">Print</span></a></li>\
+									</ul>\
+							  	</div>\
+							</div>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Edit details">\
+								<i class="la la-edit"></i>\
+							</a>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
+								<i class="la la-trash"></i>\
+							</a>\
+						';
+					},
+				},
+				{
+					targets: 4,
+					render: function(data, type, full, meta) {
+						var status = {
+							1: {'title': 'Pending', 'class': 'label-light-primary'},
+							2: {'title': 'Delivered', 'class': ' label-light-danger'},
+							3: {'title': 'Canceled', 'class': ' label-light-primary'},
+							4: {'title': 'Active', 'class': ' label-light-success'},
+							5: {'title': 'In Review', 'class': ' label-light-info'},
+							6: {'title': 'Danger', 'class': ' label-light-danger'},
+							7: {'title': 'Warning', 'class': ' label-light-warning'},
+						};
+						if (typeof status[data] === 'undefined') {
+							return data;
+						}
+						return '<span class="label label-lg font-weight-bold' + status[data].class + ' label-inline">' + status[data].title + '</span>';
+					},
+				},
+			],
+		});
+
+		$('#kt_datatable_search_status').on('change', function() {
+			datatable.search($(this).val().toLowerCase(), 'Status');
+		});
+
+		$('#kt_datatable_search_type').on('change', function() {
+			datatable.search($(this).val().toLowerCase(), 'Type');
+		});
+
+		$('#kt_datatable_search_status, #kt_datatable_search_type').selectpicker();
+	};
+
+	return {
+
+		//main function to initiate the module
+		init: function() {
+			init();
+		}
+	};
+}();
+
+jQuery(document).ready(function() {
+	KTDatatablesAdvancedColumnRenderingAdminMember.init();
+});
